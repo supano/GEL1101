50f9e93f5d4156a5507e610bd48c4e851ea87f05
supano <supano@gmail.com>
2017-04-17 15:40:12 +0700

    add page , zoom image

---
+++ app/src/main/java/com/ssru/gel1101/gel1101/LesSon1.java
@@ -0,0 +1,20 @@
+package com.ssru.gel1101.gel1101;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.github.chrisbanes.photoview.PhotoView;
+
+
+
+public class LesSon1 extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lesson1);
+
+        PhotoView photoView = (PhotoView) findViewById(R.id.photo_view);
+        photoView.setImageResource(R.drawable.work_ver4_forapp);
+    }
+}
