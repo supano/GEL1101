821d9bcfde6ee9e90fe652dba0ef63df5d5f3506
supano <supano@gmail.com>
2017-04-27 20:38:06 +0700

    add all files
    add all files

--- app/src/main/java/com/ssru/gel1101/gel1101/LesSon1.java
+++ app/src/main/java/com/ssru/gel1101/gel1101/LesSon1.java
@@ -389,17 +389,5 @@ public class LesSon1 extends Activity implements LoadImageTask.Listener {
     public void onError() {
 
     }
-    private String getRealPathFromURI(Uri contentURI) {
-        String result;
-        Cursor cursor = getContentResolver().query(contentURI, null, null, null, null);
-        if (cursor == null) { // Source is Dropbox or other similar local file path
-            result = contentURI.getPath();
-        } else {
-            cursor.moveToFirst();
-            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-            result = cursor.getString(idx);
-            cursor.close();
-        }
-        return result;
-    }
+
 }
